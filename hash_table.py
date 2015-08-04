de6e745b126aef5cc4645fdeec7092cabe6868aa
Tanner Welsh <tannerwelsh@gmail.com>
2015-08-04 14:15:29 -0400

    Make HashTable class

--- hash_table.py
+++ hash_table.py
@@ -7,71 +7,74 @@
 # - get list of values
 # - convert to list of tuples
 
-def create():
-    return [];
+class HashTable:
+    def __init__(self):
+        self.table = []
 
-def insert(key, value, hashtbl):
-    new_item = (hash(key), key, value)
-    insert_before_index = insert_index(new_item, hashtbl)
-    hashtbl.insert(insert_before_index, new_item)
-    return hashtbl
+    def insert(self, key, value):
+        new_item = (hash(key), key, value)
+        insert_before_index = self.insert_index(new_item)
+        self.table.insert(insert_before_index, new_item)
+        return self.table
 
-def get(key, hashtbl):
-    hashkey = hash(key)
-    index = get_index(hashkey, hashtbl)
-    if index == None:
-        return None
-    item = hashtbl[index]
-    return item[-1]
+    def get(self, key):
+        hashkey = hash(key)
+        index = self.get_index(hashkey)
+        if index == None:
+            return None
+        item = self.table[index]
+        return item[-1]
 
-def update(key, value, hashtbl):
-    hashkey = hash(key)
-    index = get_index(hashkey, hashtbl)
-    if index == None:
-        raise Exception('Key not found.')
-    updated_item = (hashkey, key, value)
-    hashtbl[index] = updated_item
-    return updated_item
+    def update(self, key, value):
+        hashkey = hash(key)
+        index = self.get_index(hashkey)
+        if index == None:
+            raise Exception('Key not found.')
+        updated_item = (hashkey, key, value)
+        self.table[index] = updated_item
+        return updated_item
 
-def keys(hashtbl):
-    return [item[1] for item in hashtbl]
+    def keys(self):
+        return [item[1] for item in self.table]
 
-def values(hashtbl):
-    return [item[-1] for item in hashtbl]
+    def values(self):
+        return [item[-1] for item in self.table]
 
-def to_list(hashtbl):
-    return [(item[1], item[-1]) for item in hashtbl]
+    def to_list(self):
+        return [(item[1], item[-1]) for item in self.table]
 
-def insert_index(new_item, hashtbl):
-    insert_index = 0
-    size = len(hashtbl)
-    for idx, item in enumerate(hashtbl):
-        if idx == 0 and item[0] > new_item[0]:
-            insert_index = idx
-            break
-        elif idx < size and is_between(new_item[0], hashtbl[idx - 1][0], item[0]):
-            insert_index = idx
-            break
-        elif idx == size - 1 and item[0] < new_item[0]:
-            insert_index = size
-            break
-    return insert_index
+    def insert_index(self, new_item):
+        insert_index = 0
+        size = len(self.table)
+        for idx, item in enumerate(self.table):
+            if idx == 0 and item[0] > new_item[0]:
+                insert_index = idx
+                break
+            elif idx < size and self.is_between(new_item[0], self.table[idx - 1][0], item[0]):
+                insert_index = idx
+                break
+            elif idx == size - 1 and item[0] < new_item[0]:
+                insert_index = size
+                break
+        return insert_index
 
-def is_between(item, first, second):
-    return item > first and item < second
+    def is_between(self, item, first, second):
+        return item > first and item < second
 
-def get_index(hashkey, hashtbl, offset=None):
-    if offset == None:
-        offset = 0
-    size = len(hashtbl)
-    if size == 0:
-        return None
-    mid_index = size // 2
-    mid_item = hashtbl[mid_index]
-    if hashkey == mid_item[0]:
-        return offset + mid_index
-    elif hashkey < mid_item[0]:
-        return get_index(hashkey, hashtbl[0:mid_index], offset)
-    else:
-        offset = offset + mid_index + 1
-        return get_index(hashkey, hashtbl[mid_index + 1:], offset)
+    def get_index(self, hashkey, table=None, offset=None):
+        if table == None:
+            table = self.table
+        if offset == None:
+            offset = 0
+        size = len(table)
+        if size == 0:
+            return None
+        mid_index = size // 2
+        mid_item = table[mid_index]
+        if hashkey == mid_item[0]:
+            return offset + mid_index
+        elif hashkey < mid_item[0]:
+            return self.get_index(hashkey, table[0:mid_index], offset)
+        else:
+            offset = offset + mid_index + 1
+            return self.get_index(hashkey, table[mid_index + 1:], offset)
--- hash_table_test.py
+++ hash_table_test.py
@@ -1,35 +1,40 @@
-import hash_table as htb
+from hash_table import HashTable
 
 def test():
-    htbl = htb.create()
+    htb = HashTable()
 
-    htb.insert('I', 1, htbl)
-    htb.insert('V', 5, htbl)
-    htb.insert('X', 10, htbl)
-    htb.insert('L', 50, htbl)
-    htb.insert('C', 100, htbl)
-    htb.insert('D', 500, htbl)
-    htb.insert('M', 1000, htbl)
+    htb.insert('I', 1)
+    htb.insert('V', 5)
+    htb.insert('X', 10)
+    htb.insert('L', 50)
+    htb.insert('C', 100)
+    htb.insert('D', 500)
+    htb.insert('M', 1000)
 
-    print(sorted(htbl) == htbl)
+    print(sorted(htb.table) == htb.table)
 
-    if htb.get('I', htbl) != 1:
-        print(htb.get('I', htbl))
+    if htb.get('I') != 1:
+        print(htb.get('I'))
         print(hash('I'))
-        print(htbl)
+        print(htb.table)
 
-    print(htb.get('I', htbl) == 1)
-    print(htb.get('C', htbl) == 100)
-    print(htb.get('foo', htbl) == None)
+    print(htb.get('I') == 1)
+    print(htb.get('V') == 5)
+    print(htb.get('X') == 10)
+    print(htb.get('L') == 50)
+    print(htb.get('C') == 100)
+    print(htb.get('D') == 500)
+    print(htb.get('M') == 1000)
+    print(htb.get('foo') == None)
 
-    htb.update('V', 2, htbl)
+    htb.update('V', 2)
 
-    print(htb.get('V', htbl) == 2)
+    print(htb.get('V') == 2)
 
-    print(htb.keys(htbl))
-    print(htb.values(htbl))
-
-    print(htbl)
-    print(htb.to_list(htbl))
+    # print(htb.keys())
+    # print(htb.values())
+    #
+    # print(htb.table)
+    # print(htb.to_list())
 
 test()
